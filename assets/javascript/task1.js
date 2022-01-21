b1ab4d8900c8a02934006189e8df8ef84779142c
Ethan Hammerstedt <ethan.hammerstedt@vtsd.com>
2022-01-21 07:58:38 -0500

    Java Unit Test Update 1

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,6 +1,8 @@
 /* Put code for the task below */
-function functionName(){
-	alert("Run My Stuff");
-	
-	
+function showButton(){
+	var btn = document.getElementById("buttonTask1");btn.style.display = "block";
+}
+function noImg(){
+	var img = document.getElementById("imageTask1");
+	img.style.display = "none";
 }
